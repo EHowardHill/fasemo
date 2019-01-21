50c744275f2d5fab4b00d08a005ec751980bde15
Ethan Hill <ethan.hill1999@outlook.com>
2019-01-21 16:23:09 -0600

    Update
    Update

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+import pprint, json
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return render_template('index.html')
+
+@app.route('/home', methods=['POST'])
+def home():
+    content = ""
+
+    try:
+        content = request.form['content']
+
+    except Exception as e:
+        print(str(e))
+
+    pprint.pprint(content)
     return render_template('index.html')
