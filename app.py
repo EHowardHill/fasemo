52dea3d75eb2e12027e4c57f1861545d2347b251
Ethan Hill <ethan.hill1999@outlook.com>
2019-02-25 12:40:15 -0600

    Saving is a thing!
    Yaaaaaay!

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template
+import os, json, mysql
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
+    print (os.listdir("./"))
     return render_template('index.html')
+
+@app.route('/save_work', methods=['POST'])
+def save_work():
+
+    print("Yay! We got here!")
+
+    success = 1
+    id = request.form['id']
+    content = request.form['content']
+
+    try:
+        file = open('./works/' + str(id) + ".htm", 'w+')
+        file.write(content)
+        file.close()
+
+    except Exception as e:
+        print(str(e))
+        success = 0
+
+    return json.dumps({'status': success});
