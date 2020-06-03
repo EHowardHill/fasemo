43bfd0a0cdf9493473bb55573791ea7f3dc6c900
EHowardHill <ethan.hill1999@outlook.com>
2020-06-03 12:06:07 -0500

    babel additions and react bits

--- logic/logic.py
+++ logic/logic.py
@@ -1,8 +1,8 @@
 from pprint import pprint
 from flask import Flask, render_template, request, send_file, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, DateTime
 
+# Initial DB setup
 application = Flask(__name__, template_folder='templates')
 application.secret_key = "welcome to the cheese house"          # Arbitrary, can be any string
 application.config['SESSION_TYPE'] = 'filesystem'
@@ -10,16 +10,11 @@ application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fasemo.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(application)
 
-class Login(db.Model):
-    __tablename__ = 'login'
-    usern = Column(String(45), primary_key = True)
-    passw = Column(String(45))
+# Import SQlAlchemy model classes
+from models import Alias, Alias_Reference, Login, Messages, Permissions, Thread
 
+@application.route('/', methods=["GET","POST"])
 def start():
-    info = Login.query.filter_by(usern='ethan').first()
-    if info.passw == 'Bullwinkle01':
-        print('Yay!')
-    else:
-        print('um, no!')
+    return render_template('landing.htm')
 
 start()
